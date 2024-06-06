6994ac5f92c28d87c02a3537f6abf0f084203f5f
x1141237699 <1141237699@qq.com>
2024-06-07 00:49:31 +0800

    Update OAsystemApplication.java

--- src/main/java/com/xiao/oasystem/OAsystemApplication.java
+++ src/main/java/com/xiao/oasystem/OAsystemApplication.java
@@ -3,10 +3,6 @@ package com.xiao.oasystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TODO 获取申请前鉴权
-
-//TODO 部署上线
-
 @SpringBootApplication
 public class OAsystemApplication {
 
